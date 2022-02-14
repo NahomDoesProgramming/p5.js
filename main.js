75ec63834559e055a696cba7085147c27c745d53
NahomDoesProgramming <84427393+NahomDoesProgramming@users.noreply.github.com>
2022-02-13 18:20:58 -0800

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function setup()
 {
     canvas = createCanvas(640, 480);
     canvas.position(110, 300);
+    video = createCapture(VIDEO);
+    video.hide();
 }
 function draw()
 {
@@ -15,17 +17,4 @@ function draw()
     square(120, 70, 70);
     fill(245, 127, 10);
     triangle(150, 150, 70, 150, 150, 70);
-}
-function take_snapshot()
-{
-    image_name = document.getElementById("image_name").value;
-    save(image_name + ".png");
-    if(image_name == "")
-    {
-        save("noname.png");
-    }
-}
-function filter_tint()
-{
-    tint_color = document.getElementById("tint_color").value;
 }
